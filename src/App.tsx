e8324a0b08c17e9f999e19eee194ebc3b608597c
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-04-22 17:23:54 -0300

    feat: created the study list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Form from "./components/Form";
+import Lista from "./components/Lista";
 
 function App() {
     return (
         <div className="App">
             <Form />
+            <Lista />
         </div>);
 }
 
