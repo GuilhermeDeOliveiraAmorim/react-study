df665fad8c9956773223b2a3282b0f3bb30f8669
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-04-22 18:01:32 -0300

    feat: list of studies placed in array map with key

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,26 +1,35 @@
 function Lista() {
+    const tarefas = [
+        {
+            tarefa: 'React',
+            tempo: '02:00:00'
+        },
+        {
+            tarefa: 'TypeScript',
+            tempo: '03:00:00'
+        }
+        ,
+        {
+            tarefa: 'UX/UI',
+            tempo: '01:00:00'
+        }
+    ]
     return (
         <aside>
             <h2>
                 Estudos do dia
             </h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        TypeScript
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tarefas.map((tarefa, index) => (
+                    <li key={index}>
+                        <h3>
+                            {tarefa.tarefa}
+                        </h3>
+                        <span>
+                            {tarefa.tempo}
+                        </span>
+                    </li>
+                ))}
             </ul>
         </aside>
     );
