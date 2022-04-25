dbd0e147682d7a0984b1ddedcd0b4ce6dd949baa
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-04-25 12:53:23 -0300

    feat:  stopwatch timer adjustments

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,9 +1,19 @@
-import { tempoParaSegundos } from "../../common/utils/date";
+import { useState } from "react";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefas";
 import Button from "../Button";
 import Clock from "./Clock";
 import StyleStopwatch from './Stopwatch.module.scss';
 
-export default function Stopwatch() {
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+export default function Stopwatch({selecionado} : Props) {
+    const [tempo, setTempo] = useState<number>();
+    if (selecionado?.tempo) {
+        setTempo(tempoParaSegundos(selecionado.tempo));
+    }
     return (
         <div className={StyleStopwatch.cronometro}>
             <p className={StyleStopwatch.titulo}>
